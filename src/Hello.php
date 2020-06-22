27c21327133cd880ad31e0036f7ebf46dd161def
Daan Rijpkema <info@codex.management>
2020-06-22 11:18:35 +0200

    Back to basics again

---
+++ src/Hello.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * (c) YOUR NAME <your@email.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+// If you don't to add a custom vendor folder, then use the simple class
+// namespace HelloComposer;
+namespace daanrijpkema\BluemPHP;
+
+class Hello
+{
+    public function say($toSay = "Nothing given")
+    {
+        return $toSay;
+    }
+}
