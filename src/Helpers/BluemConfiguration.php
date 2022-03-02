c6e6ba768c51a62e08309a7b36df103aeaa17d95
Peter Meester <p.meester@meesterdevelopment.nl>
2022-03-02 12:05:32 +0100

    Update BluemConfiguration.php

--- src/Helpers/BluemConfiguration.php
+++ src/Helpers/BluemConfiguration.php
@@ -5,63 +5,84 @@ namespace Bluem\BluemPHP\Helpers;
 use Bluem\BluemPHP\Validators\BluemConfigurationValidator;
 use Exception;
 
-/**
- *
- */
 class BluemConfiguration
 {
-
-    /** @var  */
+    /**
+     * @var mixed
+     */
     public $environment;
-    /** @var  */
+    
+    /**
+     * @var mixed
+     */
     public $senderID;
-    /** @var  */
+    
+    /**
+     * @var mixed
+     */
     public $brandID;
-    /** @var  */
+    
+    /**
+     * @var mixed
+     */
     public $accessToken;
-    /** @var  */
+    
+    /**
+     * @var mixed
+     */
     public $merchantReturnURLBase;
+    
     /**
      * @var BluemConfigurationValidator
      */
     private $validator;
+    
     /**
      * @var mixed
      */
     public $test_accessToken;
+    
     /**
      * @var mixed
      */
     public $IDINBrandID;
+    
     /**
      * @var mixed
      */
     public $merchantID;
+    
     /**
      * @var string
      */
     public $production_accessToken;
+    
     /**
      * @var string
      */
     public $expectedReturnStatus;
+    
     /**
      * @var string
      */
     public $eMandateReason;
+    
     /**
      * @var string
      */
     public $localInstrumentCode;
+    
     /**
      * this is given by the bank and never changed (default 0)
      * @var string
      */
     public $merchantSubID;
+    
     /**
      * @var string
      */
     private $PaymentsBrandID;
+    
     /**
      * @var string
      */
@@ -76,12 +97,13 @@ class BluemConfiguration
     {
         if (is_array($raw))
         {
-            $raw = (object)$raw;
+            $raw = (object) $raw;
         }
 
         $this->validator = new BluemConfigurationValidator();
-        $raw_validated= $this->validator->validate($raw);
-        if($raw_validated === false) {
+        
+        $raw_validated = $this->validator->validate($raw);
+        if ($raw_validated === false) {
             throw new Exception('Bluem Configuration is not valid: '.$this->errorsAsString());
         }
         
@@ -94,10 +116,10 @@ class BluemConfiguration
         
         $this->test_accessToken = $raw_validated->test_accessToken;
         
-        $this->IDINBrandID = $this->_assumeBrandID("Identity",$this->brandID);
+        $this->IDINBrandID = $this->_assumeBrandID("Identity", $this->brandID);
         // @todo: Test it
-        $this->PaymentsBrandID = $this->_assumeBrandID("Payment",$this->brandID);
-        $this->EmandateBrandID = $this->_assumeBrandID("Mandate",$this->brandID);
+        $this->PaymentsBrandID = $this->_assumeBrandID("Payment", $this->brandID);
+        $this->EmandateBrandID = $this->_assumeBrandID("Mandate", $this->brandID);
         // @todo PeterMeester: create validation step for IDINBrandID
         
         $this->merchantID = $raw_validated->merchantID;
@@ -118,15 +140,21 @@ class BluemConfiguration
      * @throws Exception
      */
     private function _assumeBrandID(string $service, string $brandID) {
-        if($brandID ==="" ) {
+        if (empty($brandID)) {
             throw new Exception("No brandID given");
         }
         
-        $available_services =  ['Identity','Payment','Mandate'];
-        if(!in_array($service, $available_services)) {
+        $available_services = [
+            'Identity',
+            'Payment',
+            'Mandate'
+        ];
+        
+        if (!in_array($service, $available_services)) {
             throw new Exception("Invalid service requested");
         }
-        $prefix = str_replace($available_services,'',$brandID);
+        
+        $prefix = str_replace($available_services, '', $brandID);
         return $prefix.ucfirst($service);
     }
     
