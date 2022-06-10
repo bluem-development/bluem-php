7ca66711f4456ea972e8837dbea54185de88f041
Daan Rijpkema <info@daanrijpkema.com>
2022-06-10 17:59:39 +0200

    Proper UTF8 encoding in request descriptions

---
+++ tests/unit/IdentityRequestTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bluem\Tests\Unit;
+
+require_once __DIR__ . '/BluemGenericTest.php';
+
+class IdentityRequestTest extends BluemGenericTest
+{
+    public function testCanCreateIdentityRequestWithWeirdCharacters() {
+ 
+        $request = $this->bluem->CreateIdentityRequest(
+            ["CustomerIDRequest","NameRequest"],
+            "Identificatie EsmeÃ© Timmerman",
+            "nope",
+            random_int(0,12353),
+            "https://google.com"
+        );
+        
+//        $this->assertInstanceOf(IdentityBluemRequest::class, $request);
+    
+        $response = $this->bluem->performRequest($request);
+
+        $this->assertEquals(false,$response->Error());
+        
+//        $transactionId = $response->GetTransactionID();
+//        $entranceCode = $response->GetEntranceCode();
+//        $status = $this->bluem->IdentityStatus(
+//            $transactionId,
+//            $entranceCode
+//        );
+//        
+        $this->assertEquals(false, $response->Error());
+
+    }
+}
