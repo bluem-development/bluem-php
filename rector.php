4f63b951757c16711cdbabb8da079e7a88336fe4
daan.rijpkema <daan.rijpkema@coolblue.nl>
2023-01-27 17:26:07 +0100

    Rector config

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -14,10 +15,11 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+//    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
-//        $rectorConfig->sets([
-//            LevelSetList::UP_TO_PHP_80
-//        ]);
+        $rectorConfig->sets([
+            LevelSetList::UP_TO_PHP_80,
+            SetList::CODE_QUALITY
+        ]);
 };
