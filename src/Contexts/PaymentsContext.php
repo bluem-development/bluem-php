b92537c967ddfbaf8d4074b2bf25c8f37fb18ecc
Peter Meester <p.meester@meesterdevelopment.nl>
2023-05-17 22:05:48 +0200

    Updated BIC list epayments

--- src/Contexts/PaymentsContext.php
+++ src/Contexts/PaymentsContext.php
@@ -37,16 +37,15 @@ class PaymentsContext extends BluemContext
                 new BIC( "ABNANL2A", "ABN AMRO" ),
                 new BIC( "ASNBNL21", "ASN Bank" ),
                 new BIC( "BUNQNL2A", "bunq" ),
-                new BIC( "HANDNL2A", "Handelsbanken" ),
                 new BIC( "INGBNL2A", "ING" ),
                 new BIC( "KNABNL2H", "Knab" ),
-                new BIC( "MOYONL21", "Moneyou" ),
                 new BIC( "RABONL2U", "Rabobank" ),
                 new BIC( "RBRBNL21", "RegioBank" ),
                 new BIC( "SNSBNL2A", "SNS" ),
                 new BIC( "TRIONL2U", "Triodos Bank" ),
                 new BIC( "FVLBNL22", "Van Lanschot" ),
                 new BIC( "REVOLT21", "Revolut" ),
+                new BIC( "BITSNL2A", "Yoursafe" ),
             ]
         );
     }
