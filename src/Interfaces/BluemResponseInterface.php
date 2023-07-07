b5bf5aa815d9bd3b08c8c3ca2842a800ab566fc3
daan.rijpkema <daan.rijpkema@coolblue.nl>
2023-07-07 12:25:32 +0200

    Added required interfaces

---
+++ src/Interfaces/BluemResponseInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * (c) 2023 - Bluem Plugin Support <pluginsupport@bluem.nl>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
+namespace Bluem\BluemPHP\Interfaces;
+
+interface BluemResponseInterface
+{
+    public function Error(): string;
+    public function ReceivedResponse(): bool;
+    public function Status(): bool;
+}
