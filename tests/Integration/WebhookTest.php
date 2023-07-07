dc549fd1a8bc1e40710c61608eb5e1a111d7c546
daan.rijpkema <daan.rijpkema@coolblue.nl>
2023-07-07 12:31:35 +0200

    Added todo's  and start of webshop tests

---
+++ tests/Integration/WebhookTest.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * (c) 2023 - Bluem Plugin Support <pluginsupport@bluem.nl>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Integration;
+
+use Bluem\BluemPHP\Requests\EmandateBluemRequest;
+
+require_once __DIR__ . '/BluemGenericTest.php';
+
+class WebhookTest extends BluemGenericTest
+{
+    public function testCanPerformWebhook()
+    {
+        $this->markTestSkipped("To be implemented");
+    }
+}
