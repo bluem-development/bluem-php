76f3f361476816234e59914f8d7f0a75bc476535
daan.rijpkema <daan.rijpkema@coolblue.nl>
2025-07-16 16:47:38 +0200

    fix: proper years in checks

--- src/Validators/WebhookSignatureValidation.php
+++ src/Validators/WebhookSignatureValidation.php
@@ -72,11 +72,11 @@ class WebhookSignatureValidation extends WebhookValidator
         $prefix = 'webhook_bluem_nl_';
 
         // 2025 certificate on production from July 18th, 8:30 CET time
-        if ($this->env === BLUEM_ENVIRONMENT_PRODUCTION && ( ( $current_date === "2025-07-18" && $current_time >= "08:30" ) || $current_date > "2023-07-18")) {
+        if ($this->env === BLUEM_ENVIRONMENT_PRODUCTION && ( ( $current_date === "2025-07-18" && $current_time >= "08:30" ) || $current_date > "2025-07-18")) {
             $timestamp = '20250717';
         // 2025 certificate on testing & acceptance from July 17th, 8:30 CET time
         } elseif (($this->env === BLUEM_ENVIRONMENT_TESTING || $this->env === BLUEM_ENVIRONMENT_ACCEPTANCE)
-            && (($current_date === "2024-07-17" && $current_time >= "6:30") || $current_date > "2024-07-17")) {
+            && (($current_date === "2025-07-17" && $current_time >= "6:30") || $current_date > "2025-07-17")) {
             $timestamp = '20250717';
         } elseif ( (  $current_date === "2024-07-01" && $current_time >= "12:00" ) || $current_date > "2024-07-01") {
             $timestamp = '20240701';
