3cc8d961aef8c07cfe4015e3ce21fd3dd4e49a44
daan.rijpkema <daan.rijpkema@coolblue.nl>
2026-04-06 16:17:05 +0200

    Add PHP_PLUGIN_VERSION constant to define the plugin version
    This commit introduces a new constant, PHP_PLUGIN_VERSION, to specify the version of the PHP plugin as "3.0". This addition helps in maintaining version control and consistency across the application.

--- src/Constants.php
+++ src/Constants.php
@@ -4,22 +4,30 @@ namespace Bluem\BluemPHP;
 
 readonly class Constants
 {
+    /**
+     * The version of this plug-in, to be updated upon each release
+     */
+    public const string PHP_PLUGIN_VERSION = "3.0.0";
+
     public const string PRODUCTION_ENVIRONMENT = "prod";
     public const string ACCEPTANCE_ENVIRONMENT = "acc";
     public const string TESTING_ENVIRONMENT = "test";
 
-    /** @var string[]  */
+    /**
+     * All available environment values
+     * @var string[]
+     */
     public const array ENVIRONMENTS = [
         self::PRODUCTION_ENVIRONMENT,
         self::ACCEPTANCE_ENVIRONMENT,
         self::TESTING_ENVIRONMENT,
     ];
 
-    // A fallback merchantID used in testing environments
+    /**
+     * A fallback merchantID used in testing environments
+     */
     public const string BLUEM_STATIC_MERCHANT_ID = "0020000387";
 
-
-    // Possible return statuses, used by validation
     public const string EXPECTED_RETURN_NONE = "none";
     public const string EXPECTED_RETURN_SUCCESS = "success";
     public const string EXPECTED_RETURN_CANCELLED = "cancelled";
@@ -28,6 +36,9 @@ readonly class Constants
     public const string EXPECTED_RETURN_OPEN = "open";
     public const string EXPECTED_RETURN_PENDING = "pending";
 
+    /**
+     * Possible return statuses, used by validation
+     */
     public const array POSSIBLE_RETURN_STATUSES = [
         self::EXPECTED_RETURN_NONE,
         self::EXPECTED_RETURN_SUCCESS,
