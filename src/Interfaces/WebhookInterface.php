7a5fee3df5a70ab45fa78e3ad719310fb1bc6aa8
Daan Rijpkema <info@daanrijpkema.com>
2026-06-29 17:01:04 +0200

    refactor: Remove PHP CodeSniffer step from CI and update exclusion rules in phpcs.xml and phpcs.xml.dist

--- src/Interfaces/WebhookInterface.php
+++ src/Interfaces/WebhookInterface.php
@@ -1,13 +1,13 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * © 2026 - Bluem Plugin Support <pluginsupport@bluem.nl>
  *
  * This source file is subject to the license that is bundled
  * with this source code in the file LICENSE.
  */
+
+declare(strict_types=1);
 
 namespace Bluem\BluemPHP\Interfaces;
 
